ecf36ce0d554e5c4949f8b1eb66a07a39b4ca060
LugarkPirog <sabaka4ka@mail.ru>
2017-01-26 19:33:52 +0300

    123

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -8,28 +8,39 @@
 using namespace std;
 
 class Field {
-	char a;
+	int count = 0;
+	char a[3][3] = {
+		0, 0, 0,
+		0, 0, 0,
+		0, 0, 0
+		// 79 = O; 88 = X
+	};
 	char fld[7][7] = {
 		char(218), char(196), char(194), char(196), char(194), char(196), char(191),
-		char(179),         a, char(179),         a, char(179),         a, char(179),
+		char(179),   a[0][0], char(179),   a[0][1], char(179),   a[0][2], char(179),
 		char(195), char(196), char(197), char(196), char(197), char(196), char(180),
-		char(179),         a, char(179),         a, char(179),         a, char(179),
+		char(179),   a[1][0], char(179),   a[1][1], char(179),   a[1][2], char(179),
 		char(195), char(196), char(197), char(196), char(197), char(196), char(180),
-		char(179),         a, char(179),         a, char(179),         a, char(179),
+		char(179),   a[2][0], char(179),   a[2][1], char(179),   a[2][2], char(179),
 		char(192), char(196), char(193), char(196), char(193), char(196), char(217)
 
 	};
 public: 
 	Field() {
-		a =(char) 23;
+		a;
 	};
 	void show() {
 		for (int i = 0; i < 7; i++) {
 			for (int j = 0; j < 7; j++) {
-				cout << (int) fld[i][j];
+				cout << fld[i][j];
 			};
 			cout << endl;
 		};
+		
+	};
+	void upd(int x, int y) {
+		a[x][y] = count % 2 ? 88 : 79;
+		count++;
 	};
 };
 
@@ -38,8 +49,8 @@ int main()
 {
 	setlocale(0, "C");
 	Field f;
-	//setlocale(LC_ALL, "C");
 	f.show();
+	for(;;)
 	system("pause");
     return 0;
 }
